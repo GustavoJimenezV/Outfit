907cac3e395409acfa7f7ba50dfd059f29cc62f8
GustavoJimenezV <dmx_gt@live.com.mx>
2019-11-19 03:20:21 -0600

    Indez y Mostrar los Datos
    Co-Authored-By: null <GustavoJimenezV@users.noreply.github.com>

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,12 +1,21 @@
 class ImagesController < ApplicationController
-    def new
 
+    def index
+        @images = Image.all
+    end
+
+    def new
     end
 
     def create
         #render plain: params[:image].inspect
         @image = Image.new image_params
         @image.save
+        redirect_to @image
+    end
+
+    def show
+        @image = Image.find params[:id]
     end
 
     def image_params
