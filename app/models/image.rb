5a983234d529fae86a7122a91fabdd7fb1f6c651
GustavoJimenezV <dmx_gt@live.com.mx>
2019-11-27 08:23:51 -0600

    Relacion de uno a muchos
    Co-Authored-By: null <GustavoJimenezV@users.noreply.github.com>

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,6 @@ class Image < ApplicationRecord
     mount_uploader :picture, PictureUploader
 
     validates :description, :season, :climate, presence: true
+
+    belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_123742) do
+ActiveRecord::Schema.define(version: 2019_11_27_134507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2019_11_27_123742) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "picture"
     t.string "climate"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_images_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +40,5 @@ ActiveRecord::Schema.define(version: 2019_11_27_123742) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "images", "users"
 end
