6004b9fed9e1df4f081ebc4a6eb0809e870ebd20
GustavoJimenezV <dmx_gt@live.com.mx>
2019-11-28 08:23:11 -0600

    Modificaciones a la Paginacion solo falta crear el Outfit
    Co-Authored-By: null <GustavoJimenezV@users.noreply.github.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,7 @@
 class HomeController < ApplicationController
   def index
       if user_signed_in?
-          Post.paginate(page:  params[:page], per_page: 12)
-        @images = Image.paginate(:page => params[:page], :per_page => 30)
+        @images = Image.paginate(page: params[:page], per_page: 12)
                         .order(id: :desc)
                         .where user_id: current_user.id
       end
